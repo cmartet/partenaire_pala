c1714afe99b83bc884c48170a2c62862b2b58338
abenoit <amelie.benoit33@gmail.com>
2016-12-29 17:43:30 +0100

    GameCards info

--- front/src/components/screens/SearchScreen.jsx
+++ front/src/components/screens/SearchScreen.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {withRouter} from 'react-router'
 import FilterBar from '../search/FilterBar'
 import GameInfo from '../search/GameInfo'
+import murtest from '../../assets/images/murtest.jpg';
 
 import './SearchScreen.scss';
 
 const SearchScreen = React.createClass({
 
     isSearchScreen(){
-        return this.props.location.pathname === '/'? " active" : "";
+        return this.props.location.pathname === '/' ? " active" : "";
     },
 
     isCreateScreen(){
@@ -26,12 +27,38 @@ const SearchScreen = React.createClass({
                     </div>
                 </div>
                 <FilterBar/>
-                <GameInfo
-                    level="3"
-                    place="Pessac"
-                    maxPlayers="4"
-                    date={new Date().toLocaleString()}
-                />
+                <div className="result-games">
+                    <GameInfo
+                        level="3"
+                        placePicture={murtest}
+                        place="Pessac"
+                        maxPlayers="4"
+                        creator="Rainbow Dash"
+                        date="2017-10-31T14:00:00.000Z"
+                        players={[{"name": "Sushiii"}, {"name": "Pinkie pie"}]}
+                    />
+                    <GameInfo
+                        level="1"
+                        place="VILLENAVE D'ORNON"
+                        maxPlayers="8"
+                        date="2016-12-29T14:00:00.000Z"
+                        players={[{"name": "Sushiii"}]}
+                    />
+                    <GameInfo
+                        level="2"
+                        place="BLA BLA BLA"
+                        maxPlayers="2"
+                        date="2016-12-30T14:00:00.000Z"
+                        players={[{"name": "Sushiii"}, {"name": "Pinkie pie"}]}
+                    />
+                    <GameInfo
+                        level="6"
+                        place="Moga"
+                        maxPlayers="4"
+                        date="2017-10-31T00:00:00.000Z"
+                        players={[{"name": "Sushiii"}, {"name": "Pinkie pie"}]}
+                    />
+                </div>
             </div>
         )
     }
--- front/src/components/search/GameInfo.jsx
+++ front/src/components/search/GameInfo.jsx
@@ -1,44 +1,162 @@
 import React, {Component} from 'react';
-import {Button} from 'react-bootstrap';
+import {Badge, Button} from 'react-bootstrap';
+import PersonIcon from '../../assets/images/ic_person_white.svg';
 
 import './GameInfo.scss';
 
 const propTypes = {
-    place: React.PropTypes.string,
-    placePicture:  React.PropTypes.string,
+    place: React.PropTypes.string.required,
+    placePicture: React.PropTypes.string,
     date: React.PropTypes.object.dateTime,
     level: React.PropTypes.bool.string,
-    maxPlayers:  React.PropTypes.number,
+    maxPlayers: React.PropTypes.number,
+    creator: React.PropTypes.bool.string,
     players: React.PropTypes.array
+};
+
+const defaultProps = {
+    placePicture: null,
+};
+
+const translateDay = [
+    "Dimanche",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi"];
+
+const translateMonth = [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre"];
+
+var areDatesSameDay = function (date1, date2) {
+    var firstDate = new Date(date1);
+    var secondDate = new Date(date2);
+
+    return firstDate.setHours(0, 0, 0, 0) === secondDate.setHours(0, 0, 0, 0);
 };
 
 class GameInfo extends Component {
 
-    gameIsOver(){
+    playersList() {
+        const listItems = this.props.players.map((player) =>
+            <Badge>{player.name}</Badge>
+        );
+        return (<div> {listItems}</div>);
+    }
+
+    getFormattedDate() {
+        if (this.gameIsToday()) {
+            return "Aujourd'hui";
+        }
+
+        if (this.gameIsTomorrow()) {
+            return "Demain";
+        }
+
+        var date = new Date(this.props.date);
+        var day = translateDay[date.getDay()];
+        var numberInMonth = date.getDate();
+        var month = translateMonth[date.getMonth()];
+        var year = date.getFullYear();
+
+        return day + " " + numberInMonth + " " + month + " " + year;
+    }
+
+    gameIsToday() {
+        var today = new Date();
+        return areDatesSameDay(this.props.date, today);
+    }
+
+    gameIsTomorrow() {
+        var today = new Date();
+        var tomorrow = today.setDate(today.getDate() + 1);
+        return areDatesSameDay(this.props.date, tomorrow);
+    }
+
+
+    getFormattedTime() {
+        var datetime = new Date(this.props.date);
+        var hour = datetime.getHours();
+        var minutes = datetime.getMinutes();
+        if (minutes.toString().length < 2) {
+            minutes = "0" + minutes;
+        }
+
+        return hour + ":" + minutes;
+    }
+
+    gameIsOver() {
         return this.props.date > new Date();
     }
 
-    gameHasEnoughPlayers(){
-        return this.props.maxPlayers >= this.props.players.length
+    gameHasEnoughPlayers() {
+        return this.props.maxPlayers >= this.props.players.length;
     }
 
-    isGameComplete(){
+    isGameComplete() {
         return this.gameIsOver || this.gameHasEnoughPlayers;
+    }
+
+    getNbPlayersInfo() {
+        return (<div><span className="nb-players-present">
+            {this.props.players.length}
+        </span>
+            <span className="nb-players-max"> /  {this.props.maxPlayers}</span>
+        </div>);
     }
 
     render() {
         return (
             <div className="GameInfo">
-                <div className="place">{this.props.place}</div>
-                <img src={this.props.placePicture}/>
-                <div className="date">{this.props.date}</div>
+                <div className="header-info">
+                    <div className="place">{this.props.place}</div>
+                    <div className="nb-players-info">
+                        <img src={PersonIcon}/>
+                        {this.getNbPlayersInfo()}</div>
+                </div>
 
-                <Button disabled={this.isGameComplete()}>Rejoindre</Button>
+                <div className="all-info">
+                      <span>
+                          Proposé par
+                          <span className="creator-name">
+                              &nbsp;{this.props.creator}
+                          </span>
+                      </span>
+
+                    {(this.props.placePicture !== null) ?
+                        <img src={this.props.placePicture} alt={this.props.place + " Picture"}/>
+                        : null}
+                    <div className="game-datetime">
+                        <div className="date">{this.getFormattedDate()}</div>
+                        <div className="time">{this.getFormattedTime()}</div>
+                    </div>
+                    <div className="players-info">
+                        <div className="players-list">Participants</div>
+                        <div className="players">
+                            {this.playersList()}
+                        </div>
+                    </div>
+                    <Button disabled={this.isGameComplete()}>Rejoindre</Button>
+                </div>
             </div>
         );
     }
 }
 
 GameInfo.protoTypes = propTypes;
+GameInfo.defaultProps = defaultProps;
 
 export default GameInfo;
