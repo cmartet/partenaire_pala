923ed4688e10b2f140980c55b81ef3a77b157d09
cmartet <chloe.martet@atinternet.com>
2017-02-06 23:17:07 +0100

    CORS now allowed

--- back/server.js
+++ back/server.js
@@ -7,6 +7,7 @@ var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var cors = require('cors');
 
 var configDB = require('./config/database.js');
 var middlewares = require('./app/helpers/middlewares.js');
@@ -18,6 +19,7 @@ var port = process.env.PORT || 8090;
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 
+app.use(cors()); // allow cross-domain requests
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
