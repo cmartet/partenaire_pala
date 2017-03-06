61d620ff1480c336e781e7b2d680239d8a3736f0
cmartet <chloe.martet@atinternet.com>
2017-03-06 13:58:14 +0100

    Fix GET places with errors handling

--- back/app/routes/places.routes.js
+++ back/app/routes/places.routes.js
@@ -3,12 +3,12 @@ var handleServiceCallback = require('./../helpers/tools').handleServiceCallback;
 
 module.exports = function (app) {
 
-    app.get('/places/search/:search', function (req, res) {
+    app.get('/places/search/:search', function (req, res, next) {
         var search = req.params.search;
         placesService.getBySearch(search, handleServiceCallback(req, res, next));
     });
 
-    app.get('/places/lat/:lat/lng/:lng/radius/:radius', function (req, res) {
+    app.get('/places/lat/:lat/lng/:lng/radius/:radius', function (req, res, next) {
         var lat = req.params.lat;
         var long = req.params.lng;
         var radius = req.params.radius;
