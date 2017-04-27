73167ffdc37e3a2e2305e8584ed535629a193bfb
Amélie Benoit <amelie.benoit33@gmail.com>
2017-04-27 13:03:40 +0200

    Fix config nodejs

--- back/app/config/config.prod.js
+++ back/app/config/config.prod.js
@@ -3,7 +3,7 @@
     "facebookAuth": {
         "clientID": "1239828719434445", // App ID
         "clientSecret": "3e91a4f236bb4620a96b4a61d7d77d9f", // App Secret Key
-        "callbackURL": "http://localhost:8090/auth/facebook/callback",
+        "callbackURL": "http://api.partenaire-pala.fr/auth/facebook/callback",
         "profileFields": ["name", "email"],
         "passReqToCallback": true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
     },
